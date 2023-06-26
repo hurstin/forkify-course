c8ca0113c2a7cb008d3f579fbb8bfd727ecaadd2
hurstin <okechukwuhurstin@gmail.com>
2023-06-26 02:16:20 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '46458887-60bd-4960-b3df-43f2a8d464f5';
+export const MODAL_CLOSE_SEC = 2.5;
